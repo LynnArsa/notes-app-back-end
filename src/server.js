5f11027d4bf95dcb34ac7a07483c7728c74f9bd6
LynnArsa <lintang13arsa@gmail.com>
2023-10-04 20:32:49 +0700

    initial commit

---
+++ src/server.js
@@ -0,0 +1,22 @@
+/* eslint-disable import/no-extraneous-dependencies */
+const Hapi = require('@hapi/hapi');
+const route = require('./routes');
+
+const init = async () => {
+  const server = Hapi.server({
+    port: 5000,
+    host: process.env.NODE_ENV !== 'production' ? 'localhost' : '0.0.0.0',
+    routes: {
+      cors: {
+        origin: ['*'],
+      },
+    },
+  });
+
+  server.route(route);
+
+  await server.start();
+  console.log(`server berjalan pada ${server.info.uri}`);
+};
+
+init();
